dabc9944783585d44e307800c2e1429ffcc43711
Vaibhav Bhasin <vaibhavbhasin15@gmail.com>
2017-07-25 16:42:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/administrator/mrmanager/Attendance.java
@@ -0,0 +1,28 @@
+package com.example.administrator.mrmanager;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+
+public class Attendance extends MainActivity{
+    LinearLayout dynamicContent,bottonNavBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendence);
+
+        dynamicContent = (LinearLayout)  findViewById(R.id.dynamicContent);
+        bottonNavBar= (LinearLayout) findViewById(R.id.bottonNavBar);
+        View wizard = getLayoutInflater().inflate(R.layout.activity_upload_notes, null);
+        dynamicContent.addView(wizard);
+
+
+        //get the reference of RadioGroup.
+        RadioButton rb=(RadioButton)findViewById(R.id.uploadnotes);
+
+        // Change the corresponding icon and text color on nav button click.
+        rb.setTextColor(Color.parseColor("#3F51B5"));
+    }
+}
---
+++ app/src/main/java/com/example/administrator/mrmanager/UploadNotes.java
@@ -0,0 +1,30 @@
+package com.example.administrator.mrmanager;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+
+public class UploadNotes extends MainActivity {
+    LinearLayout dynamicContent,bottonNavBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_upload_notes);
+
+        dynamicContent = (LinearLayout)  findViewById(R.id.dynamicContent);
+        bottonNavBar= (LinearLayout) findViewById(R.id.bottonNavBar);
+        View wizard = getLayoutInflater().inflate(R.layout.activity_upload_notes, null);
+        dynamicContent.addView(wizard);
+
+
+        //get the reference of RadioGroup.
+        RadioButton rb=(RadioButton)findViewById(R.id.uploadnotes);
+
+        // Change the corresponding icon and text color on nav button click.
+        rb.setTextColor(Color.parseColor("#3F51B5"));
+    }
+}
