a430c918db4ae42c37fac9227b986f149e736aac
Vaibhav Bhasin <vaibhavbhasin15@gmail.com>
2017-07-31 11:05:14 +0530

    laptop

--- app/src/main/java/com/example/administrator/mrmanager/UploadNotes.java
+++ app/src/main/java/com/example/administrator/mrmanager/UploadNotes.java
@@ -1,8 +1,11 @@
 
 package com.example.administrator.mrmanager;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -10,6 +13,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,8 +22,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 
 public class UploadNotes extends MainActivity implements View.OnClickListener {
@@ -27,18 +34,20 @@ public class UploadNotes extends MainActivity implements View.OnClickListener {
     Button btn_Upload,btn_Reset;
     TextView text_fileName;
     String selected_class;
+    String topic;
     int FILE_SELECT_CODE=1;
     private StorageReference mStorageRef;
     Spinner spinner_class,spinner_subject;
     Uri selectedFileUri;
     Uri downloadUrl1;
+    EditText txt_topic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_notes);
 
-        mStorageRef = FirebaseStorage.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference().child("Notes");
 
         // Change the corresponding icon and text color on nav button click.
 
@@ -48,7 +57,7 @@ public class UploadNotes extends MainActivity implements View.OnClickListener {
         btn_Upload.setOnClickListener(this);
         btn_Reset = (Button) findViewById(R.id.Button_Reset);
         btn_Reset.setOnClickListener(this);
-
+        txt_topic = (EditText) findViewById(R.id.txt_topic);
         text_fileName = (TextView) findViewById(R.id.Text_FileName);
 
         spinner_class = (Spinner) findViewById(R.id.Spinner_Class);
@@ -69,11 +78,12 @@ public class UploadNotes extends MainActivity implements View.OnClickListener {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
             {
                 select();
-            } // to close the onItemSelected
-            public void onNothingSelected(AdapterView<?> parent)
-            {
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
+            // to close the onItemSelected
         });
     }
     public void select() {
@@ -108,7 +118,8 @@ public class UploadNotes extends MainActivity implements View.OnClickListener {
         }
         else if(view == btn_Upload)
         {
-            Upload(selectedFileUri);
+            topic = txt_topic.getText().toString().trim();
+           new HitUpload().execute();
         }
         else if(view == btn_Reset)
         {
@@ -116,28 +127,54 @@ public class UploadNotes extends MainActivity implements View.OnClickListener {
         }
     }
 
-    private void Upload(Uri SFU)
+    class HitUpload extends AsyncTask<Void, Void, Void>
     {
+        ProgressDialog progressDialog = new ProgressDialog(UploadNotes.this);
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.show();
+            progressDialog.setMessage("Uploading..");
+        }
 
-        StorageReference pdfRef = mStorageRef.child("images/rivers.jpg");
+        @Override
+        protected Void doInBackground(Void... voids) {
 
-        pdfRef.putFile(SFU)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // Get a URL to the uploaded content
-                        @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                        downloadUrl1=downloadUrl;
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle unsuccessful uploads
-                        // ...
-                    }
-                });
+            StorageReference pdfRef = mStorageRef.child(selectedFileUri.getLastPathSegment());
+
+            final String path = pdfRef.getPath();
+
+            pdfRef.putFile(selectedFileUri).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    @SuppressWarnings("VisibleForTests")  double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    int currentprogress = (int) progress;
+                    progressDialog.setMessage("Uploading.. "+currentprogress);
+
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+                            @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                            downloadUrl1=downloadUrl;
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            Toast.makeText(getApplicationContext(),"Some Error Occured",Toast.LENGTH_LONG).show();
+                        }
+                    });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -145,7 +182,8 @@ public class UploadNotes extends MainActivity implements View.OnClickListener {
         if(requestCode==FILE_SELECT_CODE)
         {
             try {
-                selectedFileUri = data.getData();
+                Uri selectedFileUri1 = data.getData();
+                selectedFileUri = Uri.fromFile(new File(selectedFileUri1.toString().trim()));
                 text_fileName.setText(selectedFileUri.toString().trim());
             }catch (Exception e){
                 Toast.makeText(getApplicationContext(),"No File Selected",Toast.LENGTH_SHORT);
@@ -159,3 +197,26 @@ public class UploadNotes extends MainActivity implements View.OnClickListener {
         finish();
     }
 }
+
+
+    /*private void Upload(Uri SFU,String topic)
+    {
+
+        StorageReference pdfRef = mStorageRef.child("notes/"+topic);
+
+        pdfRef.putFile(SFU)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        downloadUrl1=downloadUrl;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(getApplicationContext(),"Some Error Occured",Toast.LENGTH_LONG);
+                    }
+                });
+    }*/
